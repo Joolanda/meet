708704dca066c0d32af30b5efa629786e235aacd
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-01 23:08:07 +0200

    setup tests folder

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+
+describe('<App /> component', () => {
+//test('renders learn react link', () => {
+//  const { getByText } = render(<App />);
+//  const linkElement = getByText(/learn react/i);
+//  expect(linkElement).toBeInTheDocument();
+});
