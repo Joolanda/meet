5ee857a9d7718595c7d8fac0ae62b823db663acb
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-01 23:36:31 +0200

    mk EventList component, add in App and Test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import { render } from '@testing-library/react';
+import EventList from './EventList';
 
 class App extends Component {
   render() {
     return (
       < div className="App">
-
+        <EventList />
       </div>
     );
   }
