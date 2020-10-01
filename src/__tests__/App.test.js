122963610ba8b418f2baae3cfb7d07fcc6807aa8
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-01 23:44:09 +0200

    import EventList

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
-import EventList from '.EventList';
+import EventList from '../EventList';
 
 describe('<App /> component', () => {
-
   test('render list of events', () =>{
     const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   });
-//test('renders learn react link', () => {
-//  const { getByText } = render(<App />);
-//  const linkElement = getByText(/learn react/i);
-//  expect(linkElement).toBeInTheDocument();
 });
