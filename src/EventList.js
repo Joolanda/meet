e941061c195dd3953159147ca21787d976e0db23
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-02 14:31:51 +0200

    setting up test for EventList

--- src/EventList.js
+++ src/EventList.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import Event from './Event';
 
 class EventList extends Component {
+  state = {
+    events: [],
+  }
   render() {
     return (
-      <div>        
-      </div>
+      <ul className="EventList"> 
+      {this.state.events.map(event =>
+        <li>
+          <Event event={event} />
+        </li>)}
+      </ul>
     );
   }
 }
-
 export default EventList;
