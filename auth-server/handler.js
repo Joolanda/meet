1f635c913878276c49618cecc93f3747c632981e
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-05 21:22:12 +0200

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -97,8 +97,7 @@ module.exports.getCalendarEvents = async (event) => {
       // calendar_id[0]
   );
 
-  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
-  //const access_token = decodeURIComponent(`${event.pathParameters.code}`);
+  const access_token = decodeURIComponent(event.pathParameters.access_token);
 
   oAuth2Client.setCredentials({ access_token });
 
