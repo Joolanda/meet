e391017e2839c5641490ae937821663968849320
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-06 22:51:10 +0200

    edditing and update mockdata

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -7,6 +7,6 @@ describe('<App /> component', () => {
   test('render correct number of events', () => {
     const EventListWrapper = shallow(<EventList />);
     EventListWrapper.setState({ events: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }] });
-    expect(EventListWrapper.find(Event)).toHaveLength(2);
+    expect(EventListWrapper.find(Event)).toHaveLength(4);
   });
 });
