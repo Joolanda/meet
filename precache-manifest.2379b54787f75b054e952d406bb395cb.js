29203488a8ecc1022cd7a03875360b03c9c37664
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-07 18:06:08 +0200

    Updates

--- precache-manifest.2379b54787f75b054e952d406bb395cb.js
+++ precache-manifest.2379b54787f75b054e952d406bb395cb.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a8219fd575a4df74e6f5320771c62b27",
+    "revision": "d3c3cbbc317e72b551eea92727cdf81d",
     "url": "/meet/index.html"
   },
   {
-    "revision": "15ae94bcb7cf290573b4",
+    "revision": "a05f378308cfe9c8c3d8",
     "url": "/meet/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/meet/static/js/2.96aef505.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "15ae94bcb7cf290573b4",
-    "url": "/meet/static/js/main.f287949a.chunk.js"
+    "revision": "a05f378308cfe9c8c3d8",
+    "url": "/meet/static/js/main.235a2379.chunk.js"
   },
   {
     "revision": "9de4f142a6170bcd9b6d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meet/precache-manifest.b571c5d98445c3617942f51b836cbe7b.js"
+  "/meet/precache-manifest.2379b54787f75b054e952d406bb395cb.js"
 );
 
 self.addEventListener('message', (event) => {
