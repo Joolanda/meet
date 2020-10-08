7733d00a9526de49ca55116054239e5553a1fc12
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-08 15:57:13 +0200

    write updateEvents function

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ import Event from './Event';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents } from './api';
 
-
-
-
 class App extends Component {
+state = {
+  events: [],
+  currentLocation: 'all',
+  locations: [],
+}
+
+updateEvents = (location) => {
+
+  getEvents().then((response) => {
+    const locationEvents =
+      location === 'all'
+        ? response.events
+        : response.events.filter((event) => event.location === location);
+    const events = locationEvents.slice(0, numberOfEvents);
+    return this.setState({
+      events : events,
+      currentLocation: location,
+    });
+  });
+}
   render() {
+
     return (
       < div className="App">
         <CitySearch updateEvents={this.updateEvents}/>
@@ -20,5 +38,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
