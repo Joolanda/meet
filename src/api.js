33188efa087fdcd8bb0855eaff6b5b2a5f4e8808
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-12 00:40:20 +0200

    api integration with ext host

--- src/api.js
+++ src/api.js
@@ -1,36 +1,37 @@
 import { mockData } from './mock-data';
+import NProgress from "nprogress";
 import axios from 'axios';
 
-/* const accessToken = localStorage.getItem('access_token');
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+    window.location.protocol + "//" + 
+    window.location.host +
+    window.location.pathname;
+  window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host,
+    window.history.pushState("", "", newurl);
+  }
+};
 
-const tokenCheck = accessToken && {await checkToken(accessToken);
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem('access_token');
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get('code');
-    if (!code) {
-      const results = await axios.get(
-        "YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT"
-        // const getAuthURL = "https://jvsv8khfpd.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url";
-        // const getToken = "https://jvsv8khfpd.execute-api.us-east-1.amazonaws.com/dev/api/token/";
-      );
-      const { authUrl } = results.data;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-    }
-    return accessToken; */
+//  call a function, checkToken, to check the token’s validity
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
 
-    const checkToken = async (accessToken) => {
-      const result = await fetch(
-        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-        )
-        .then((res) => res.json())
-        .catch((error) => error.json());
-      
-        return result;
-    };
-
+  return result;
+};
+    // some keys, check them later
+    //"YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT"
+    // const getAuthURL = "https://jvsv8khfpd.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url";
+    //
+    // const getToken = "https://jvsv8khfpd.execute-api.us-east-1.amazonaws.com/dev/api/token/";
+    // YOUR_GET_EVENTS_API_ENDPOINT
+    // 
 /**
  *
  * @param {*} events:
@@ -39,38 +40,78 @@ const tokenCheck = accessToken && {await checkToken(accessToken);
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
  */
-export const extractLocations = (events) => {
+
+const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
   var locations = [...new Set(extractLocations)];
   return locations;
 };
 
-
-export const getEvents = async () => {
+const getEvents = async () => {
   NProgress.start();
-  if (window.location.href.startsWith('http://localhost')) {
+
+  if (window.location.href.startsWith("http://localhost")) {
     NProgress.done();
-  return { events: mockData, locations: extractLocations(mockData) };
-  };
-};
+    return { events: mockData, locations: extractLocations(mockData) };
+  }
 
-const token = await getAccessToken();
+  const token = await getAccessToken();
 
-if (token) {
-  removeQuery();
-  const url = `YOUR_GET_EVENTS_API_ENDPOINT/${token}`;
-  const result = await axios.get(url);
-  if (result.data) {
-    var locations = extractLocations(result.data.events);
-    localStorage.setItem('lastEvents', JSON.stringify(result.data));
-    localStorage.setItem('locations', JSON.stringify(locations));
+  if (token) {
+    removeQuery();
+    const url = `YOUR_GET_EVENTS_API_ENDPOINT/${token}`;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+    }
     NProgress.done();
     return { events: result.data.events, locations };
   }
 };
 
 
-// export const extractSummaries = (events) => {
-//  var extractSummaries = events.map((event) => event.summary);
-//   var summaries = [...new Set(extractSummaries)];
-// return summaries;};
+// const getAccessToken Waar is deze functie gebleven?
+const accessToken = localStorage.getItem('access_token');
+
+//  google redirect user back to app with code which you can use
+// to retrieve the access tocken from Lambda fcts on auth server.
+const tokenCheck = accessToken && (await checkToken(accessToken));
+
+if (!accessToken || tokenCheck.error) {
+  await localStorage.removeItem("access_token");
+  const searchParams = new URLSearchParams(window.location.search);
+  const code = await searchParams.get("code");
+  if (!code) {
+    const results = await axios.get(
+      //"YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT"
+      "https://jvsv8khfpd.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+    );
+    const { authUrl } = results.data;
+    return (window.location.href = authUrl);
+  }
+  return code && getToken(code);
+}
+return accessToken;
+
+
+// check volgorde van fcts
+
+
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    `YOUR_GET_ACCESS_TOKEN_ENDPOINT/${encodeCode}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
+
+  access_token && localStorage.setItem("access_token", access_token);
+
+  return access_token;
+};
+
+export { getEvents, getAccessToken, extractLocations, getToken, checkToken };
