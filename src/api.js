0c65460d920010c13e029fa9899988c76426b627
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-11 22:13:57 +0200

    ao integrating final api

--- src/api.js
+++ src/api.js
@@ -1,4 +1,36 @@
 import { mockData } from './mock-data';
+import axios from 'axios';
+
+/* const accessToken = localStorage.getItem('access_token');
+
+const tokenCheck = accessToken && {await checkToken(accessToken);
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem('access_token');
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get('code');
+    if (!code) {
+      const results = await axios.get(
+        "YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT"
+        // const getAuthURL = "https://jvsv8khfpd.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url";
+        // const getToken = "https://jvsv8khfpd.execute-api.us-east-1.amazonaws.com/dev/api/token/";
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+    }
+    return accessToken; */
+
+    const checkToken = async (accessToken) => {
+      const result = await fetch(
+        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+        )
+        .then((res) => res.json())
+        .catch((error) => error.json());
+      
+        return result;
+    };
+
 /**
  *
  * @param {*} events:
@@ -13,9 +45,30 @@ export const extractLocations = (events) => {
   return locations;
 };
 
+
 export const getEvents = async () => {
+  NProgress.start();
+  if (window.location.href.startsWith('http://localhost')) {
+    NProgress.done();
   return { events: mockData, locations: extractLocations(mockData) };
+  };
 };
+
+const token = await getAccessToken();
+
+if (token) {
+  removeQuery();
+  const url = `YOUR_GET_EVENTS_API_ENDPOINT/${token}`;
+  const result = await axios.get(url);
+  if (result.data) {
+    var locations = extractLocations(result.data.events);
+    localStorage.setItem('lastEvents', JSON.stringify(result.data));
+    localStorage.setItem('locations', JSON.stringify(locations));
+    NProgress.done();
+    return { events: result.data.events, locations };
+  }
+};
+
 
 // export const extractSummaries = (events) => {
 //  var extractSummaries = events.map((event) => event.summary);
