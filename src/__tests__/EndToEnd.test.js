8c2fcf65ae0f7db928a329ab28fbfa5065e035d7
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-15 19:44:20 +0200

    update end-to-end test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,6 +1,9 @@
 import puppeteer from 'puppeteer';
 
 describe('show/hide an event details', () => {
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+  })
   test('An event element is collapsed by default', async () =>
   {
     const browser = await puppeteer.launch();
@@ -14,5 +17,19 @@ describe('show/hide an event details', () => {
 
     expect(eventDetails).toBeNull();
     browser.close();
+  });
+
+test('User van expand an event to see its details', async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000/');
+
+  await page.waitForSelector('.event');
+  await page.click('.event .details-btn');
+  
+  const eventDetails = await page.$('.event .event__Details');
+  
+  expect(eventDetails).toBeDefined();
+  browser.close();
   });
 });
