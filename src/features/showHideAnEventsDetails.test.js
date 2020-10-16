41d55120a94f0c7ffbf80dafc27e60c9c096675e
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-16 20:21:57 +0200

    update

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,6 +1,6 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 
-const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
+const feature = loadFeature('./src/features/showOrHideAnEventsDetails.feature');
 
 defineFeature(feature, test => {
 
