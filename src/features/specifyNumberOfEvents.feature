415566f42cc2e7e8f0ee2aaa33848be6dd2d306f
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-17 18:53:09 +0200

    edditing some text

--- src/features/specifyNumberOfEvents.feature
+++ src/features/specifyNumberOfEvents.feature
@@ -1,9 +1,9 @@
 Feature: Specify Number of events
 
-Scenario: When user hasn’t specified a number, 32 is the default number
+Scenario: When user hasn’t specified a number, "32" is the default number
 Given the user sees his selected city with a textbox showing default amount of events
 When user submits without changing the number in the “Show-Events” textbox 
-Then the user should see a selection of 32 upcoming events in that city
+Then the user should see a selection of "32" upcoming events in that city
 
 Scenario: User can change the number of events they want to see
 Given the user sees the “Show Events” textbox and the user did not specify a number of events he wants to see
---
+++ src/features/specifyNumberOfEvents.test.js
@@ -0,0 +1,34 @@
+import { loadFeature, defineFeature } from 'jest-cucumber';
+
+const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
+
+defineFeature(feature, test => {
+  test('When user hasn’t specified a number, "32" is the default number', ({ given, when, then }) => {
+    given('the user sees his selected city with a textbox showing default amount of events', () => {
+
+    });
+
+    when('user submits without changing the number in the “Show-Events” textbox', () => {
+
+    });
+
+    then('the user should see a selection of "32" upcoming events in that city',() => {
+
+    });
+  });
+
+  test('User can change the number of events they want to see', ({ given, when, then }) => {
+    given('the user sees the “Show Events” textbox and the user did not specify a number of events he wants to see', () => {
+
+    });
+
+    when('the user is typing “a number” in the “Show Events” textbox', () => {
+
+    });
+
+    then('the user should receive a list of this number of events in that city', () => {
+
+    });
+  });
+
+});
