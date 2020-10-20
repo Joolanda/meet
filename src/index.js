25efdab4ae52c335b21d3975eb980d02fd020920
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-20 13:53:48 +0200

    setup atatus

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as atatus from 'atatus-js';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+atatus.config('YOUR_UNIQUE_ATATUS_CONFIG_CODE').install();
+atatus.notify(new Error('Test Atatus Setup'));
