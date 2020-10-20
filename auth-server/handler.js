f4e878d711c49f7d5ee29fea9773182c320ca199
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-20 14:51:35 +0200

    clean up

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -84,7 +84,7 @@ module.exports.getAccessToken = async (event) => {
          return {
            statusCode: 500,
            headers: {
-           'Access-Control-Allow-Origin': '*',
+            "Access-Control-Allow-Origin": "*",
          },
            body: JSON.stringify(err),
          };
