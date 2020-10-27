6175ef96a2b8a961bf28223a03be8d32ba413fe3
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-10-27 23:49:48 +0100

    Update Handler to pass CORS

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -13,7 +13,7 @@ const credentials = {
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://joolanda.github.io/meet/"],
-  javascript_origins: ["https://joolanda.github.io", "http://localhost:3000", "*"]
+  javascript_origins: ["https://joolanda.github.io", "https://joolanda.github.io/meet","http://localhost:3000", "*"] https://joolanda.github.io
   };
 
   const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
