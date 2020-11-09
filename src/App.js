84240e342860f97bb98ac3febc33cd248027830a
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-11-09 21:11:16 +0100

    update CitySearch (Oop, Alert)

--- src/App.js
+++ src/App.js
@@ -81,8 +81,8 @@ updateEvents = (location, eventCount) => {
         <h1> Meet App</h1>
         <h3>Choose your nearest city</h3>
         <CitySearch updateEvents={this.updateEvents} locations={locations} />
-        <EventList events={events} />
         <NumberOfEvents updateEvents={this.updateEvents} numberOfEvents= {numberOfEvents} />
+        <EventList events={events} />
       </div>
     );
   }
--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 //const locations = extractlocations(mockData);
 
@@ -6,8 +7,8 @@ class CitySearch extends Component {
   state = {
     locations: this.props.locations,
     query: '', 
+    suggestions: [],
     showSuggestions: false,
-    suggestions: []
   };
 
 
@@ -17,17 +18,26 @@ class CitySearch extends Component {
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    return this.setState({
-      query: value,
-      showSuggestions: true,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We can not find the city you are looking for. Please try another city',
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText:''
+      });
+    }
   };
+
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
       suggestions: [],
       showSuggestions: false,
+      infoText:''
     });
     this.props.updateEvents(suggestion);
  };
@@ -35,6 +45,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input
           type='text'
           className='city'
