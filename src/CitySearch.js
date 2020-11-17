4ae49c862df3dc2e1d1dce0e196032b929fccf92
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-11-17 18:59:58 +0100

    All tests (except for EndToEnd) passed
    beautify

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -61,7 +61,7 @@ class CitySearch extends Component {
           value={this.state.query}
           onChange={this.handleInputChanged}
           onFocus={() => { this.setState({ showSuggestions: true }) }}
-/>
+        />
         <ul className='suggestions' style={this.state.showSuggestions ? {} : { display: 'none' }} >
           {
            this.state.suggestions.map((suggestion) => (
