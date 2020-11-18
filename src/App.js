2e0e5d80cc30e2e2587862c0ef7ee424893ece91
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-11-18 21:39:39 +0100

    create eventGenre and add Pie Chart

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import { getEvents } from './api';
 import { WarningAlert } from './Alert';
 import { checkToken, getToken } from './api';
 import {
-  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer 
 } from 'recharts';
+import EventGenre from './EventGenre';
 
 class App extends Component {
 
@@ -98,31 +99,32 @@ render() {
     <div className="App">
       <h1>Meet App</h1>
       <h4>Choose your nearest city</h4>
-      <CitySearch updateEvents={this.updateEvents} locations={locations} />
-      <NumberOfEvents
-        updateEvents={this.updateEvents}
-        numberOfEvents={numberOfEvents}
-      />       
-       <h4>Events in each city</h4>
-
-      <ResponsiveContainer height={400} >
-        <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-          <CartesianGrid />
-          <XAxis type="category" dataKey="city" name="city" />
-          <YAxis
-            allowDecimals={false}
-            type="number"
-            dataKey="number"
-            name="number of events"
-          />
-          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-          <Scatter data={this.getData()} fill="#8884d8" />
-        </ScatterChart>
-      </ResponsiveContainer>
-      <EventList events={events} />
-    </div>
-  );
-}
+       <CitySearch updateEvents={this.updateEvents} locations={locations} />
+       <NumberOfEvents
+          updateEvents={this.updateEvents}
+          numberOfEvents={numberOfEvents}
+          />   
+          <div classname="data-vis-wrapper">  
+           <EventGenre events={events} />
+           <ResponsiveContainer height={400} >
+             <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis
+                allowDecimals={false}
+                type="number"
+                dataKey="number"
+                name="number of events"
+              />
+              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>  
+        <EventList events={events} />
+      </div>
+    );
+  }
 }
 
 export default App;
